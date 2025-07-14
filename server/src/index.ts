981b18a394d66aff0c0da83e899a1c070fc4ca9b
Ann Granian <annobongo@gmail.com>
2025-07-14 16:32:51 +0300

    fixed cors problem

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ import path from 'path';
 const app: Express = express();
 
 
-const allowedOrigin = "https://blog-it-full-stack-project.vercel.app"; // your Vercel frontend URL
+const allowedOrigin = "https://blog-it-full-stack-project.vercel.app";
 
 app.use(cors({
   origin: allowedOrigin,
